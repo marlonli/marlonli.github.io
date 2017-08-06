b6fdcb75c0745d0bf3222b7867d983d3be968000
Jingyuan Li <jingyuanli94@gmail.com>
2017-08-06 11:04:29 -0400

    Update index.html/main.js

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    $('nav a').on('click', function() {
+        $('nav a').on('click', function(){ 
+            if($('.navbar-toggle').css('display') !='none'){
+                $(".navbar-toggle").trigger( "click" );
+            }
+        });
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 64;
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 300);
+
+        return false;
+
+    })
+
+
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 1) {
+            $('.container section').each(function(i) {
+                if ($(this).position().top <= windscroll - 20) {
+                    $('nav li.active').removeClass('active');
+                    $('nav li').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            $('nav').removeClass('fixed');
+            $('nav li.active').removeClass('active');
+            $('nav li:first').addClass('active');
+        }
+
+    }).scroll();
+
+
+
+
+});
